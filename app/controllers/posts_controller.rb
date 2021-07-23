b3b7459f7eec70a9c18c6bce9facf8687b5eeb00
yoshidanoryota <yoshidanoryota777x@gmail.com>
2021-07-23 19:16:52 +0900

    コントローラーにリクエストを送信する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
 
   def create
     Post.create(content: params[:content])
-    redirect_to action: :index  # 追記する
+    redirect_to action: :index  
   end
-  
+
 end
